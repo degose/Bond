3eed56f9375c22c4d35387b736d73e117aaa96f0
Taehoon Kim <taehoon@MacBook-Pro-holjjug-teochiba.local>
2017-08-02 19:56:21 +0900

    로그인페이지를 제외한 공통의 상단 네비바 완성/

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template lang="pug">
   #app
-    
-    feed
-    //- router-view(name="home")   
+    .container
+      nav.navbar
+        .navbar-brand
+          a.navbar-item(href="#")
+            picture
+              source.responsive-object(srcset='./assets/logo-02.svg', media='max-width: 767px')
+              img(src='./assets/logo-02.svg', alt='본드', width=112, height=28)
+          .navbar-burger.burger(data-target="navMenuExample")
+            figure
+              img.image.is-30x30.user-img(src='http://bulma.io/images/placeholders/96x96.png', alt='Image', width=30, height=30)
+        .search.column
+          .field
+            .control.has-icons-left.has-icons-right
+              input.input.is-warning(type='text', placeholder='그룹이나 게시글을 검색해보세요')
+              a.span.icon.is-small.is-right(aria-label="search")
+                i.fa.fa-search
+          //- fieldset
+          //-   legend 검색 폼
+          //-   input#search-keyword(type='search', aria-label='검색어 입력상자', required='', placeholder='검색어를 입력하세요')
+            
+          //- .field
+          //-   .control.is-4-mobile
+          //-     input.input.is-warning(type='text', placeholder='Warning input')
+        #navMenuExample.navbar-menu    
+          .navbar-end
+            .navbar-item.has-dropdown.is-hoverable
+              a.navbar-link
+                figure
+                  img.image.is-30x30.user-img(src='http://bulma.io/images/placeholders/96x96.png', alt='Image')
+              .navbar-dropdown
+                a.navbar-item
+                  | 내 정보
+                a.navbar-item
+                  | 내 글 보기
+                a.navbar-item
+                  | 새 글 보기
+                hr.dropdownhr
+                a.navbar-item
+                  | 로그 아웃
+    hr.navhr
 
-    //- 직접 에셋 속성을 설정하는 경우, assets/ 디렉토리에서 찾아야 이미지 출력
-    //- file-loader를 통해 아래와 같이 처리되기 때문
-    //- /dist/logo.png?82b9c7a5a3f405032b1db71a25f67021
-    //- img(src="./assets/logo.png", :alt="vue.label")
 </template>
 
 <script>
@@ -41,5 +74,13 @@ export default {
 
 body
   // background: #ffc107
-
+.user-img
+  border-radius: 50%
+.navbar-burger.burger
+  padding-top: 8px
+  padding-left: 10px
+.navhr
+  margin: 5px
+.dropdownhr
+  margin: 5px
 </style>
