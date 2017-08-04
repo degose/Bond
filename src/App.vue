ea198fce651ee19f61059aa1d5d2640b9868bf75
chamsinnim <hansunguk6178@gmail.com>
2017-08-04 15:25:34 +0900

    paskage.json 불러오기 전

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template lang="pug">
   #app
-     
-    feed
+
 </template>
 
 <script>
 import Vue from 'Vue';
-import Home from './components/Home/Home';
-// import HeaderUser from './components/HeaderUser';
-import Feed from './components/Feed/Feed';
-
 export default {
   name: 'app',
   components: {
@@ -19,15 +14,10 @@ export default {
   },
   data () {
     return {
-      vue: {
-        // 동적 속성 바인딩 시에는 src/ 디렉토리에서 찾아야 이미지 출력
-        // file-loader를 사용하지 않고, 직접 속성 값을 설정하기 때문
-        path: './src/assets/logo.png',
-        label: 'Vue.js'
+
       }
     }
   }
-}
 </script>
 
 <style lang="sass">
@@ -35,6 +25,5 @@ export default {
 @import "~style"
 
 body
-  // background: #ffc107
 
 </style>
