d2b9c8ab38666d8af295b6e0ce3a7e35ff603901
Taehoon Kim <taehoon@MacBook-Pro-holjjug-teochiba.local>
2017-08-05 23:53:42 +0900

    내 설정 보기 view 최종 완성

--- src/components/Home/MysettingModal.vue
+++ src/components/Home/MysettingModal.vue
@@ -6,68 +6,41 @@
         p.modal-card-title 내 정보
       section.modal-card-body
         nav.level
-          .level-left
-            .level-item
-              p.subtitle.is-5
-                strong 내이름
-            .level-item
-              .field.has-addons
-                p.control
-                  input.input(type='text', placeholder='이름 변경')
-                p.control
-                  button.button
-                    | 변경           
+          .level-item.has-text-centered
+            figure.image.is-128x128
+              img.profilepic(src='http://bulma.io/images/placeholders/128x128.png')
         nav.level
-          .level-left
-            .level-item
-              p.subtitle.is-5
-                strong 닉네임
-            .level-item
-              .field.has-addons
-                p.control
-                  input.input(type='text', placeholder='닉네임 변경')
-                p.control
-                  button.button
-                    | 변경
-        nav.level          
-          .level-left
-            .level-item
-              p.subtitle.is-5
-                strong 이메일
-            .level-item
-              .field.has-addons
-                p.control
-                  input.input(type='email', placeholder='이메일 변경')
-                p.control
-                  button.button
-                    | 변경
-
-
-
+          .level-item.has-text-centered
+            .file.is-primary.is-small
+              label.file-label
+                input.file-input(type='file', name='resume')
+                span.file-cta
+                  span.file-icon
+                    i.fa.fa-upload
+                  span.file-label
+                    | Choose a file…
+        nav.level
+          .level-item.has-text-centered
+            .field.has-addons
+              p.control
+                input.input(type='text', placeholder='현재 내이름' disabled)           
+        nav.level
+          .level-item.has-text-centered
+            .field.has-addons
+              p.control
+                input.input(type='email', placeholder='현재 내 이메일' disabled)
+        nav.level
+          .level-item.has-text-centered
+            .field.has-addons
+              p.control
+                input.input(type='text', placeholder='닉네임 변경')
+              p.control
+                button.button
+                  | 변경
       footer.modal-card-foot
         button.button.is-primary 설정 변경
         button.button(@click="closeModal") 취소
-  
-      //-   columns
-      //-     div.content
-      //-       figure.image.is-128x128
-      //-         img(src='http://bulma.io/images/placeholders/128x128.png')
-      //-     .file.is-primary.is-small
-      //-       label.file-label
-      //-         input.file-input(type='file', name='resume')
-      //-         span.file-cta
-      //-           span.file-icon
-      //-             i.fa.fa-upload
-      //-           span.file-label
-      //-             | Choose a file…
-      //-     //- hr
-      //-   .field
-      //-     .control
-      //-     p 이름
-      //-       input.input.is-primary(type='text', placeholder='그룹11 이름을 설정해주세요')
-      //-       input.input.is-primary(type="text" rows="50" placeholder="그룹에 소개글을 적어주세요")
-
-
+      // level-right 요소로 우측정렬 가능.. 빈 요소라도 level-left는 언제나 필수! 
 </template>
 
 <script>
@@ -95,5 +68,6 @@ export default {
 }
 </script>
 <style lang="sass" scoped>
-
+.profilepic
+  border-radius: 50%
 </style>
