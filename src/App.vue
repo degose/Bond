b45622cd9dd2f7ba568d99b6b1aaf18ae6348e77
chamsinnim <hansunguk6178@gmail.com>
2017-08-07 15:34:11 +0900

    컴포넌트 변경 전

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template lang="pug">
   #app
-
+    .container
+      router-view
+      app-nav
+    footer.footer.is-hidden-mobile
+      .content.has-text-centered
+        p
+          strong 본드
+          |  by 
+          a(href='http://school.fastcampus.co.kr/') 패스트 캠퍼스.
+          |  웹 프론트엔드 스쿨 
 </template>
 
 <script>
-import Vue from 'Vue';
+import Navigation from './components/Navigation';
 export default {
   name: 'app',
   components: {
-    // HeaderMain,
-    Home,
-    Feed
+    appNav: Navigation
   },
   data () {
     return {
-
       }
     }
   }
@@ -23,7 +29,19 @@ export default {
 <style lang="sass">
 @import "~bulma"
 @import "~style"
-
-body
-
+html,body
+    font-size: 100%
+    margin: 0
+    padding: 0
+    height: 100%
+#app 
+    position: relative
+    min-height: 100%
+.title
+    margin-top: 20px
+.footer
+    position: absolute
+    bottom: 0
+    width: 100%
+    background-color: hsla(0, 0%, 100%, .3)
 </style>
