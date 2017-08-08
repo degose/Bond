cc8acec329bc7ed549217d80d4e7e88fa221d7e7
Taehoon Kim <taehoon@MacBook-Pro-holjjug-teochiba.local>
2017-08-08 12:51:12 +0900

    컴포넌트 분리 작업 및 정리 진행중

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,15 @@
   #app
     .container
     MainHeader
-      GroupList
-    footer.footer.is-hidden-mobile
-      .content.has-text-centered
-        p
-          strong 본드
-          |  by 
-          a(href='http://school.fastcampus.co.kr/') 패스트 캠퍼스.
-          |  웹 프론트엔드 스쿨 
-</template>
+    GroupList
+//-     footer.footer.is-hidden-mobile
+//-       .content.has-text-centered
+//-         p
+//-           strong 본드
+//-           |  by 
+//-           a(href='http://school.fastcampus.co.kr/') 패스트 캠퍼스.
+//-           |  웹 프론트엔드 스쿨 
+//- </template>
 
 <script>
 // import Navigation from './components/Navigation';
@@ -48,4 +48,6 @@ html,body
 //     bottom: 0
 //     width: 100%
 //     background-color: hsla(0, 0%, 100%, .3)
+
 </style>
+
