22fc1747faeed40cee7cfcd3ddac0f32d5c55da0
gose <gom0367@gmail.com>
2017-08-25 02:12:13 +0900

    calc 함수 수정한 최종

---
+++ src/components/Main/SearchResult.vue
@@ -0,0 +1,31 @@
+<template lang="pug">  
+  div.all-wrapper(v-cloak)
+    main-header
+    my-search
+    main-footer
+</template>
+
+<script>
+import MainHeader from '../Header-Footer/MainHeader';
+import MainFooter from '../Header-Footer/MainFooter';
+import MySearch from './MySearch';
+
+export default {
+  components: {
+    MainHeader,
+    MainFooter,
+    MySearch
+  },
+}
+
+
+</script>
+
+<style lang="sass" scoped>
+@import "~bulma"
+@import "~style"
+
+.all-wrapper
+  background: #eee
+
+</style>
